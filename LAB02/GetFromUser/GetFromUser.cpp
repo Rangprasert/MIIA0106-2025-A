a8d2068877b0bd903b50f6f432c72d9a3007560d
6811830015 <6811830015@mut.ac.th>
2025-11-25 19:33:51 +0700

    Get From User

---
+++ LAB02/GetFromUser/GetFromUser.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    float height;
+    cout << "Enter your name: ";
+    getline(cin, name);   
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Enter your height (cm): ";
+    cin >> height;
+    cout << endl;   
+    cout << "Student Profile" << endl;
+    cout << "----------------------" << endl;
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << " cm" << endl;
+
+    return 0;
+}
