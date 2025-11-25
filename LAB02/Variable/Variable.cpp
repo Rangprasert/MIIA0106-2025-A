3167234ddf1a4e2e002018c3d0a6f907d2c8f198
6811830015 <6811830015@mut.ac.th>
2025-11-25 19:50:38 +0700

    Variable

--- LAB02/Variable/Variable.cpp
+++ LAB02/Variable/Variable.cpp
@@ -6,13 +6,13 @@ int main() {
     int age = 21;
     string Student_id = "6811830015";
     string name = "Theerapat Rangprasert";
-    float GPS = 2.88
+    float GPA = 2.88;
 
     cout << "Student Profile" << endl;
     cout << "----------------------" << endl;
     cout << "Name: " << name << endl;
     cout << "Age: " << age << endl;
     cout << "Student id: " << Student_id << endl;
-
+    cout << "GPA: " << GPA << endl;
     return 0;
 }
