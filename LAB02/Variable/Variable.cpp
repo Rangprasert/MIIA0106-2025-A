5ddd7c22949e2becff5a50b7702c2b50bcad4c0a
6811830015 <6811830015@mut.ac.th>
2025-11-25 19:34:54 +0700

    Variable

---
+++ LAB02/Variable/Variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age = 21;
+    string Student_id = "6811830015";
+    string name = "Theerapat Rangprasert";
+
+    cout << "Student Profile" << endl;
+    cout << "----------------------" << endl;
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Student id: " << Student_id << endl;
+
+    return 0;
+}
