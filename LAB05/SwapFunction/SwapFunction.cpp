541b4925274d7a127ad785db414c0f2e37aa2435
6811830015 <6811830015@mut.ac.th>
2025-12-16 16:20:45 +0700

    LAB05

--- LAB05/SwapFunction/SwapFunction.cpp
+++ LAB05/SwapFunction/SwapFunction.cpp
@@ -3,20 +3,22 @@
 #include <string>
 using namespace std;	
  
-int a = 10,b = 20;
-void BeforeSwap() {
+int a,b ;
+void Swap() {
+	cout << "Enter first number: ";
+	cin >> a;
+	cout << "Enter second number: ";
+	cin >> b;
 	cout << "Before swapping a is " << a <<" b is "<< b << endl;
-}
-void AfterSwap() {
 	int temp;
 	temp = a;
 	a = b;
 	b = temp;
-	cout << "After swapping a is " << a << " b is "<< b << endl;
+	cout << "After swapping a is " << a << " b is " << b << endl;
+
 }
+
 int main() {
-	BeforeSwap();
-	AfterSwap();
-	
+	Swap();
 	return 0;
 }
